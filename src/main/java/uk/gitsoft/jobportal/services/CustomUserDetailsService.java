186837ba413f6dbb4e8d5952559ed7acec3a51f5
Bogdan <bduchnowski@gmail.com>
2025-09-10 13:33:09 +0800

    Add custom user authentication with Spring Security integration
    Set up CustomUserDetails and CustomUserDetailsService for authentication. Updated WebSecurityConfig to include authentication provider and BCrypt password encoder.

---
+++ src/main/java/uk/gitsoft/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package uk.gitsoft.jobportal.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import uk.gitsoft.jobportal.entity.Users;
+import uk.gitsoft.jobportal.repository.UsersRepository;
+import uk.gitsoft.jobportal.util.CustomUserDetails;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersService usersService;
+    private final UsersRepository usersRepository;
+    @Autowired
+    public CustomUserDetailsService(UsersService usersService, UsersRepository usersRepository) {
+        this.usersService = usersService;
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+     Users users = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(users);
+    }
+}
