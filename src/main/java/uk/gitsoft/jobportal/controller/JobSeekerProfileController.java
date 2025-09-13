b28cebc389ae9eafaf484a08aa7d81f767625a48
Bogdan <bduchnowski@gmail.com>
2025-09-13 13:56:14 +0800

    Add file upload functionality to job seeker profile creation
    - Introduced `@PostMapping("/addNew")` in `JobSeekerProfileController` to handle profile creation with file uploads.
    - Enabled support for uploading images and PDFs for job seeker profiles.
    - Updated model attributes to reflect profile and associated skills during creation.

--- src/main/java/uk/gitsoft/jobportal/controller/JobSeekerProfileController.java
+++ src/main/java/uk/gitsoft/jobportal/controller/JobSeekerProfileController.java
@@ -8,7 +8,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import uk.gitsoft.jobportal.entity.JobSeekerProfile;
 import uk.gitsoft.jobportal.entity.Skills;
 import uk.gitsoft.jobportal.entity.Users;
@@ -31,18 +34,36 @@ public class JobSeekerProfileController {
         this.usersRepository = usersRepository;
     }
 
+    @PostMapping("/addNew")
+    public String addNew(JobSeekerProfile jobSeekerProfile,
+                         @RequestParam("image") MultipartFile image,
+                         @RequestParam("pdf") MultipartFile pdf, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            jobSeekerProfile.setUserId(user);
+            jobSeekerProfile.setUserAccountId(user.getUserId());
+        }
+        List<Skills> skillsList = new ArrayList<>();
+        model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skillsList);
+
+        return "redirect:/dashboard";
+    }
+
     @GetMapping("/")
-    public String JobsiseekerProfile(Model model){
+    public String JobsiseekerProfile(Model model) {
         JobSeekerProfile jobSeekerProfile = new JobSeekerProfile();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         List<Skills> skills = new ArrayList<>();
 
-        if(!(authentication instanceof AnonymousAuthenticationToken)){
-            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(()->new UsernameNotFoundException("User not found"));
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
             Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(user.getUserId());
-            if(seekerProfile.isPresent()){
+            if (seekerProfile.isPresent()) {
                 jobSeekerProfile = seekerProfile.get();
-                if(jobSeekerProfile.getSkills().isEmpty()){
+                if (jobSeekerProfile.getSkills().isEmpty()) {
                     skills.add(new Skills());
                     jobSeekerProfile.setSkills(skills);
                 }
@@ -53,4 +74,6 @@ public class JobSeekerProfileController {
 
         return "job-seeker-profile";
     }
+
+
 }
